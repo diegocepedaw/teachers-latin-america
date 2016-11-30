5067547e79b863f94120b59f2047863daa66c8dd
Jason Kim <jason.kim1002578@gmail.com>
2016-11-30 00:16:26 -0500

    create logo and change styling

--- index2.php
+++ index2.php
@@ -52,18 +52,23 @@ $db ='id171775_tla';
 
         <title>Liquid Slider : The Last Responsive Content Slider You'll Ever Need</title>
       </head>
-      <body class="no-js green lighten-4">
+      <body class="no-js green lighten-5">
+        <div class="navbar-fixed">
+          <nav class="green darken-3">
+            <div class="container">
+              <div class="nav-wrapper">
+                <a href="#" class="brand-logo center"><img src="./images/tla-logo.png"></a>
+              </div>
+            </div>
+          </nav>
+        </div>
+
         <div class="container">
-
-
-
-
-              <div class="row">
-                   <div id="wrapper" class="col s8 offset-s2">
+            <div class="row">
+              <div class="section">
+                <div class="col 8 offset-s2">
                     <div id="main-slider" class="liquid-slider " >
                         <?php
-
-
                          if(isset($_POST['teaching_area_id']) && isset($_POST['grade_level_id'])){
                             while ($row = $query->fetch_assoc()){
                             $group_arr[] = $row;
@@ -171,108 +176,109 @@ $db ='id171775_tla';
                          }
                          ?>
                     </div>
-               </div>
-                           <form name="myForm" action="index2.php" method="post">
-      					<div class="input-field col s8 offset-s2">
-      						<select name="teaching_area_id[]"  multiple="multiple">
-      							<option value="" disabled selected>Choose subjects</option>
-      							<option value="1" >Activities Coordinator</option>
-      							<option value="2" >AP</option>
-      							<option value="3">Art</option>
-      							<option value="4">Assistant Principal</option>
-      							<option value="5">Biology</option>
-      							<option value="6">Business</option>
-      							<option value="7">Chemistry</option>
-      							<option value="8">Classroom Teacher</option>
-      							<option value="9">Counselor/Psychologist</option>
-      							<option value="10">Curriculum Coordinator</option>
-      							<option value="11">Department Head</option>
-      							<option value="12">Drama</option>
-      							<option value="13">Economics/Business</option>
-      							<option value="14">English Language Arts</option>
-      							<option value="15">ESL/EFL</option>
-      							<option value="16">Foreign/Modern Languages</option>
-      							<option value="17">Geography</option>
-      							<option value="18">History</option>
-      							<option value="19">IB DP</option>
-      							<option value="20">IB MYP</option>
-      							<option value="21">IB PYP</option>
-      							<option value="22">IGCSE</option>
-      							<option value="23">Info Tech/Computers</option>
-      							<option value="24">Interns</option>
-      							<option value="25">Library</option>
-      							<option value="26">Library</option>
-      							<option value="27">Maths</option>
-      							<option value="28">Music</option>
-      							<option value="29">Nurse</option>
-      							<option value="30">Physical Education</option>
-      							<option value="31">Physics</option>
-      							<option value="32">Principal</option>
-      							<option value="33">Psychology</option>
-      							<option value="34">Science</option>
-      							<option value="35">Social Science</option>
-      							<option value="36">Sociology/Psychology</option>
-      							<option value="37">Special Education/Resources</option>
-      							<option value="38">Theory of Knowledge</option>
-      						</select>
-      						<label>Please select the positions you are trying to fill.</label>
-      					</div>
-
-      					<div class="input-field col s8 offset-s2">
-      						<select  name="grade_level_id[]"  multiple="multiple">
-      							<option value="" disabled selected>Choose grade levels</option>
-      							<option value="1">Pre-School</option>
-      							<option value="2">Primary School</option>
-      							<option value="3">Middle School</option>
-      							<option value="4">Secondary School</option>
-      							<option value="5">Tertiary School</option>
-      						</select>
-      						<label>Please select the grade levels you would like your candidate to teach.</label>
-      					</div>
-
-                              <div class="input-field col s8 offset-s2">
-      						<select  name="ib[]"  multiple="multiple">
-      							<option value="" disabled selected>Choose grade levels</option>
-      							<option value="0">Yes</option>
-
-      						</select>
-      						<label>IB Teacher</label>
-      					</div>
-
-                              <div class="input-field col s8 offset-s2">
-      						<select  name="experience[]"  multiple="multiple">
-      							<option value="" disabled selected>Choose grade levels</option>
-      							<option value="19">Yes</option>
-
-      						</select>
-      						<label>More than two years experience</label>
-      					</div>
-
-                              <div class="input-field col s8 offset-s2">
-      						<select  name="certified[]"  multiple="multiple">
-      							<option value="" disabled selected>Choose grade levels</option>
-      							<option value="0">Yes</option>
-      						</select>
-      						<label>Certified</label>
-      					</div>
-
-                            <button class="btn waves-effect waves-light col s4 offset-s4" type="submit" value="Submit" name="action">Search for Candidates</button>
-                      </form>
-
-      			</div>
-
-              <div class="row">
-        				<div class="col s8 offset-s2">
-
                 </div>
-
-
               </div>
-            </div>
-          </div>
+
+                <div class="section">
+                  <div class="col s8 offset-s2">
+                  <form name="myForm" action="index2.php" method="post" class="card-panel">
+                      <div class="row">
+              					<div class="input-field col s12">
+              						<select name="teaching_area_id[]"  multiple="multiple">
+              							<option value="" disabled selected>Choose subjects</option>
+              							<option value="1" >Activities Coordinator</option>
+              							<option value="2" >AP</option>
+              							<option value="3">Art</option>
+              							<option value="4">Assistant Principal</option>
+              							<option value="5">Biology</option>
+              							<option value="6">Business</option>
+              							<option value="7">Chemistry</option>
+              							<option value="8">Classroom Teacher</option>
+              							<option value="9">Counselor/Psychologist</option>
+              							<option value="10">Curriculum Coordinator</option>
+              							<option value="11">Department Head</option>
+              							<option value="12">Drama</option>
+              							<option value="13">Economics/Business</option>
+              							<option value="14">English Language Arts</option>
+              							<option value="15">ESL/EFL</option>
+              							<option value="16">Foreign/Modern Languages</option>
+              							<option value="17">Geography</option>
+              							<option value="18">History</option>
+              							<option value="19">IB DP</option>
+              							<option value="20">IB MYP</option>
+              							<option value="21">IB PYP</option>
+              							<option value="22">IGCSE</option>
+              							<option value="23">Info Tech/Computers</option>
+              							<option value="24">Interns</option>
+              							<option value="25">Library</option>
+              							<option value="26">Library</option>
+              							<option value="27">Maths</option>
+              							<option value="28">Music</option>
+              							<option value="29">Nurse</option>
+              							<option value="30">Physical Education</option>
+              							<option value="31">Physics</option>
+              							<option value="32">Principal</option>
+              							<option value="33">Psychology</option>
+              							<option value="34">Science</option>
+              							<option value="35">Social Science</option>
+              							<option value="36">Sociology/Psychology</option>
+              							<option value="37">Special Education/Resources</option>
+              							<option value="38">Theory of Knowledge</option>
+              						</select>
+              						<label>Please select the positions you are trying to fill.</label>
+              					</div>
+                      </div>
+
+                      <div class="row">
+              					<div class="input-field col s12">
+              						<select  name="grade_level_id[]"  multiple="multiple">
+              							<option value="" disabled selected>Choose grade levels</option>
+              							<option value="1">Pre-School</option>
+              							<option value="2">Primary School</option>
+              							<option value="3">Middle School</option>
+              							<option value="4">Secondary School</option>
+              							<option value="5">Tertiary School</option>
+              						</select>
+              						<label>Please select the grade levels you would like your candidate to teach.</label>
+              					</div>
+                      </div>
 
 
+                      <div class="row">
+                        <div class="col s12">
+                          <p>
+                            <input name="ib[]" type="checkbox" id="ib" />
+                            <label for="ib">IB Teacher</label>
+                          </p>
+              					</div>
+                      </div>
 
+                      <div class="row">
+                          <div class="col s12">
+                            <p>
+                              <input name="experience[]" type="checkbox" id="exp" />
+                              <label for="exp">More than two years of experience</label>
+                            </p>
+                					</div>
+              				</div>
+
+
+                      <div class="row">
+                        <div class="col s12">
+                          <p>
+                            <input name="certified[]" type="checkbox" id="cert" />
+                            <label for="cert">Certified</label>
+                          </p>
+                        </div>
+                      </div>
+
+
+                  </form>
+                  <button class="btn waves-effect waves-light col s6 offset-s3" type="submit" value="Submit" name="action">Search for Candidates</button>
+                </div>
+              </div>
+      		</div>
+      </div>
 
 
 
